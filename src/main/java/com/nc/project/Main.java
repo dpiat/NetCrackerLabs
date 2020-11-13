a08be0ba2feade33f91fed5d41a7f36214bcac29
dpiat <dpiataikin@gmail.com>
2020-11-13 14:42:32 +0300

    add CSVReader

--- src/main/java/com/nc/project/Main.java
+++ src/main/java/com/nc/project/Main.java
@@ -5,6 +5,7 @@ import com.nc.project.model.client.ClientFactory;
 import com.nc.project.model.container.Repository;
 import com.nc.project.model.contract.ContractFactory;
 import com.nc.project.model.contract.Contract;
+import com.nc.project.util.CSVReader;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -17,50 +18,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         String csvFilePath = "src\\main\\resources\\contracts.csv";
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFilePath));
-            Repository<Contract> contractRepository = new Repository<>();
-            ClientFactory clientFactory = new ClientFactory();
-            ContractFactory contractFactory = new ContractFactory();
-            String line = bufferedReader.readLine(); // skip first row
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] values = line.split(",");
-                String[] startContract = values[2].split("-");
-                String[] endContract = values[3].split("-");
-                String[] birthday = values[6].split("-");
-                Contract contract = contractFactory.getContract(
-                        Long.parseLong(values[0]),
-                        Long.parseLong(values[1]),
-                        new Date(
-                                Integer.parseInt(startContract[2]),
-                                Integer.parseInt(startContract[1]),
-                                Integer.parseInt(startContract[0])
-                        ),
-                        new Date(
-                                Integer.parseInt(endContract[2]),
-                                Integer.parseInt(endContract[1]),
-                                Integer.parseInt(endContract[0])
-                        ),
-                        clientFactory.getClient(
-                                Long.parseLong(values[4]),
-                                values[5],
-                                new Date(
-                                        Integer.parseInt(birthday[2]),
-                                        Integer.parseInt(birthday[1]),
-                                        Integer.parseInt(birthday[0])
-                                ),
-                                values[7],
-                                Long.parseLong(values[8])
-                        ),
-                        values[9]
-                );
-                contractRepository.add(contract);
-            }
-            System.out.println(contractRepository.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Repository<Contract> contractRepository = new Repository<>();
+        CSVReader.readCSV(csvFilePath, contractRepository);
+        System.out.println(contractRepository.toString());
     }
 }
---
+++ src/main/java/com/nc/project/util/CSVReader.java
@@ -0,0 +1,69 @@
+package com.nc.project.util;
+
+import com.nc.project.model.client.ClientFactory;
+import com.nc.project.model.container.Repository;
+import com.nc.project.model.contract.Contract;
+import com.nc.project.model.contract.ContractFactory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+public class CSVReader {
+
+    private CSVReader() {};
+
+    /**
+     * Reads from csv file
+     *
+     * @param csvFilePath - path of csv file
+     * @param contractRepository - repository for storage of contracts
+     */
+    public static void readCSV(String csvFilePath, Repository<Contract> contractRepository) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFilePath));
+            ClientFactory clientFactory = new ClientFactory();
+            ContractFactory contractFactory = new ContractFactory();
+            String line = bufferedReader.readLine(); // skip first row
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] values = line.split(",");
+                String[] startContract = values[2].split("-");
+                String[] endContract = values[3].split("-");
+                String[] birthday = values[6].split("-");
+                Contract contract = contractFactory.getContract(
+                        Long.parseLong(values[0]),
+                        Long.parseLong(values[1]),
+                        new Date(
+                                Integer.parseInt(startContract[2]),
+                                Integer.parseInt(startContract[1]),
+                                Integer.parseInt(startContract[0])
+                        ),
+                        new Date(
+                                Integer.parseInt(endContract[2]),
+                                Integer.parseInt(endContract[1]),
+                                Integer.parseInt(endContract[0])
+                        ),
+                        clientFactory.getClient(
+                                Long.parseLong(values[4]),
+                                values[5],
+                                new Date(
+                                        Integer.parseInt(birthday[2]),
+                                        Integer.parseInt(birthday[1]),
+                                        Integer.parseInt(birthday[0])
+                                ),
+                                values[7],
+                                Long.parseLong(values[8])
+                        ),
+                        values[9]
+                );
+                contractRepository.add(contract);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
